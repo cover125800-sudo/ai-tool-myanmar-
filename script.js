603ff368e73c87a817224368b2518b4efc840487
Ai myanmar <cover125800@gmail.com>
2026-07-02 01:11:21 +0630

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,28 @@ function renderMarkdown(text) {
 
   setTimeout(() => {
     document.querySelectorAll("pre code").forEach((block) => {
-      hljs.highlightElement(block);
+      document.querySelectorAll("pre").forEach((pre) => {
+  if (!pre.querySelector(".copy-btn")) {
+    const btn = document.createElement("button");
+    btn.className = "copy-btn";
+    btn.innerText = "📋 Copy";
+
+    btn.onclick = () => {
+      navigator.clipboard.writeText(pre.querySelector("code").innerText);
+      btn.innerText = "✅ Copied";
+      setTimeout(() => {
+        btn.innerText = "📋 Copy";
+      }, 2000);
+    };
+
+    pre.style.position = "relative";
+    pre.appendChild(btn);
+  }
+});
+
+document.querySelectorAll("pre code").forEach((block) => {
+  hljs.highlightElement(block);
+});
     });
   }, 0);
 
